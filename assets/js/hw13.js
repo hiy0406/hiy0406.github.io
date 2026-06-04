f5714d00570c67b761d332faf23e6b0ed78b1add
Inyeong Huh <120291503+hiy0406@users.noreply.github.com>
2026-06-04 18:37:42 +0000

    custom-stop 추가

--- assets/js/hw13.js
+++ assets/js/hw13.js
@@ -60,8 +60,9 @@ Promise.all([
     fetch("/data/frankenstein.txt").then(r => r.text()),
     fetch("/data/dracula.txt").then(r => r.text()),
     fetch("/data/stopwords-en.txt").then(r => r.text()),
-]).then(([frankText, dracText, stopwordsText]) => {
-    const stopwords = stopwordsText.split(/\s+/).filter(w => w.length > 0);
+    fetch("/data/stopwords-custom.txt").then(r => r.text()),
+]).then(([frankText, dracText, baseStop, customStop]) => {
+    const stopwords = (basestop + customStop).split(/\s+/).filter(w => w.length > 0);
     const frankTop = analyze(frankText, stopwords);
     const dracTop = analyze(dracText, stopwords);
     drawChart("#chart-frankenstein", frankTop, "rgba(40, 167, 69, 0.6)");
